b91c05f22dbd2bcb880ab991d605cc36ddc3e594
Parham Alvani <parham.alvani@gmail.com>
2016-02-25 16:52:03 +0330

    Some issues were corrected.

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 # In The Name Of God
 # ========================================
 # [] File Name : init.sh
@@ -18,15 +18,16 @@ fi
 if [ $? ]; then
     echo "copyrighter has been installed successfully"
 fi
-python3 -c "import airspeed"
-if [ $? ]; then
+python3 -c "import airspeed" 2> /dev/null
+if [ $? -eq 0 ]; then
 	exit
 fi
 cd ..
 echo "installing airspeed dependency"
-if [[ "$OSTYPE" == "darwin"* ]]; then
+if [ "$OSTYPE" == "darwin"* ]; then
 	pip3 install airspeed
 else
+	sudo apt-get install python3-pip
 	git clone https://github.com/purcell/airspeed.git
 	cd airspeed
 	sudo python3 setup.py install
