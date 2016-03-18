6a7ed3c8f7c0811a5af2e2c1a4b427d571013a54
Parham Alvani <parham.alvani@gmail.com>
2016-03-18 18:07:02 +0330

    Setup script was improved.

--- copyrighter/setup.py
+++ copyrighter/setup.py
@@ -8,14 +8,14 @@ for header in os.listdir('conf/header/'):
 
 setup(
     name='copyrighter',
-    version='2.1',
-    scripts=['src/scripts/copyrighter.py'],
+    version='2.2',
+    scripts=['scripts/copyrighter.py'],
     data_files=[
         (os.path.expanduser("~/.copyrighter"), ["conf/config.ini"]),
         (os.path.expanduser("~/.copyrighter/header"), headers)
     ],
     url='https://github.com/1995parham/vim-header',
-    license='GPLv2',
+    license='LICENSE.txt',
     author='Parham Alvani',
     author_email='parham.alvani@gmail.com',
     description='Advance-Dummy source codes header generator',
