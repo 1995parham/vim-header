0fab39fff013fcc26ba4343075b76c9eec4e4a21
Parham Alvani <parham.alvani@gmail.com>
2017-08-11 23:09:57 +0430

    Adds basic configuration.

---
+++ conf/header/C.c
@@ -0,0 +1,13 @@
+/*
+ * In The Name Of God
+ * ========================================
+ * [] File Name : {file}
+ *
+ * [] Creation Date : {time:%d-%m-%Y}
+ *
+ * [] Created By : {user.name} <{user.email}>
+ * =======================================
+*/
+/*
+* Copyright (c) {time: %Y} {user.name}.
+*/
---
+++ conf/header/Python.py
@@ -0,0 +1,8 @@
+# In The Name Of God
+# ========================================
+# [] File Name : {file}
+#
+# [] Creation Date : {time:%d-%m-%Y}
+#
+# [] Created By : {user.name} <{user.email}>
+# =======================================
