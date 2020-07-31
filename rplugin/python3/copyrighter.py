68bd39fdb19eee544e3d589e93e4d1d42883a9a4
Parham Alvani <parham.alvani@gmail.com>
2020-07-31 09:47:02 +0430

    fix: Remove Lint Issues

--- rplugin/python3/copyrighter.py
+++ rplugin/python3/copyrighter.py
@@ -7,75 +7,90 @@
 # [] Created By : Parham Alvani (parham.alvani@gmail.com)
 # =======================================
 
-import neovim
 import configparser
 import os
 import datetime
-from collections import namedtuple
+import dataclasses
+import typing
 
-User = namedtuple('User', ['name', 'email'])
+import neovim  # type: ignore
+
+
+@dataclasses.dataclass
+class User:
+    """
+    User contains information about the file owner
+    """
+
+    name: str
+    email: str
 
 
 @neovim.plugin
 class CopyrighterPlugin:
     def __init__(self, nvim):
-        # NVIM instance
+        # neovim instance
         self.nvim = nvim
 
-        # Configurations (config.ini)
+        # configurations (config.ini)
         self.headers = {}
-        cnfg_path = os.path.join(os.path.dirname(__file__), '../../conf')
+        cnfg_path = os.path.join(os.path.dirname(__file__), "../../conf")
         cnfg = configparser.ConfigParser()
         cnfg.read(os.path.join(cnfg_path, "config.ini"))
         for section in cnfg.sections():
-            for extension in cnfg[section]['Extension'].split(' '):
+            for extension in cnfg[section]["Extension"].split(" "):
                 header = open(
-                        os.path.join(cnfg_path, cnfg[section]['File']),
-                        'r')
+                    os.path.join(cnfg_path, cnfg[section]["File"]), "r"
+                )
                 self.headers[extension] = header
 
     def _context(self, srcfile: str):
-        '''
+        """
         Fills context for creating header
-        '''
+        """
 
-        context = {}
+        context: typing.Dict[str, typing.Any] = {}
 
-        context['='] = '='
-        context['-'] = '-'
-        context['*'] = '*'
-        context['#'] = '#'
+        context["="] = "="
+        context["-"] = "-"
+        context["*"] = "*"
+        context["#"] = "#"
 
-        context['user'] = User(name=self.nvim.eval('g:header_name'),
-                               email=self.nvim.eval('g:header_email'))
-        context['file'] = os.path.basename(srcfile)
-        context['time'] = datetime.datetime.now()
+        context["user"] = User(
+            name=self.nvim.eval("g:header_name"),
+            email=self.nvim.eval("g:header_email"),
+        )
+        context["file"] = os.path.basename(srcfile)
+        context["time"] = datetime.datetime.now()
 
         return context
 
-    @neovim.command('Header', eval='@%')
-    def header_command(self, srcfile: str, sync=True):
-        '''
+    @neovim.command("Header", eval="@%")
+    def header_command(self, srcfile: str):
+        """
         Provides header in existing file
-        '''
+        """
         if os.path.splitext(srcfile)[-1] in self.headers:
             extention_header = self.headers[os.path.splitext(srcfile)[-1]]
 
             context = self._context(srcfile)
 
-            lines = [l.format(**context) for l in
-                     extention_header.read().splitlines()]
+            lines = [
+                line.format(**context)
+                for line in extention_header.read().splitlines()
+            ]
             self.nvim.current.buffer.append(lines, 0)
 
             extention_header.seek(0)
 
-    @neovim.autocmd('BufNewFile', pattern=r'*',
-                    eval='expand("<afile>")', sync=True)
+    @neovim.autocmd(
+        "BufNewFile", pattern=r"*", eval='expand("<afile>")', sync=True
+    )
     def on_bufnew(self, srcfile: str):
-        '''
+        """
         Provides header in the buffer of filename
         based on it's extention.
-        '''
+        """
         if os.path.splitext(srcfile)[-1] in self.headers:
             extention_header = self.headers[os.path.splitext(srcfile)[-1]]
 
@@ -85,7 +100,7 @@ class CopyrighterPlugin:
             for line in extention_header.read().splitlines():
                 line = line.format(**context)
                 self.nvim.current.buffer[-1] = line
-                self.nvim.current.buffer.append('')
+                self.nvim.current.buffer.append("")
                 i += 1
             extention_header.seek(0)
             self.nvim.current.window.cursor = (i, 0)
